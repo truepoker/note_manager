044b4fd28db4a79d61efc7d91e8bbab428c4951e
truepoker <159036137+truepoker@users.noreply.github.com>
2025-01-09 22:48:46 +0700

    Create greetings.py

---
+++ greetings.py
@@ -0,0 +1,13 @@
+# Создание переменных
+username = 'Truepoker'
+title = 'Grade 1'
+content = 'Этап 1: Задание 1'
+status = 'Выполнено'
+created_date = '09-01-2025'
+issue_date = '11-01-2025'
+print('Имя пользователя: ',username)
+print('Заголовок заметки: ',title)
+print('Описание заметки: ',content)
+print('Статус заметки: ',status)
+print('Дата создания заметки: ',created_date)
+print('Дата истечения заметки (дедлайн): ',issue_date)
