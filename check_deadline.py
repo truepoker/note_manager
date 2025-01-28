b1f47d83c360384a664c7e3307ee4c08df60cdcf
truepoker <159036137+truepoker@users.noreply.github.com>
2025-01-28 22:32:12 +0700

    Update check_deadline.py

--- check_deadline.py
+++ check_deadline.py
@@ -1,17 +1,20 @@
+#подключение библиотеки для работы с датой и временем
 from datetime import datetime
-
+#запись текущей даты
 d = datetime.today()
-print(d.today())
 print('Текущая дата: ', d.strftime('%d-%m-%Y'))
 flag = True
 while flag :
+#проверка корректности ввода даты
     try:
         issue_date = datetime.strptime(input('Введите дату дедлайна (в формате день-месяц-год): '),'%d-%m-%Y')
     except:
         print('Неверный формат ввода даты')
     else:
         flag = False
+#разница междутекущей датой и датой дедлайна
 delta = d.date()-issue_date.date()
+#проверка сколько дней осталось до дедлайна, или дата просрочена
 if delta.days>0 : print('Дедлайн истек ' + str(delta.days) + ' дня назад')
 elif delta.days<0 : print('До дедлайн осталось ' + str(abs(delta.days)) + ' дня')
 else : print('Дедлайн сегодня')
